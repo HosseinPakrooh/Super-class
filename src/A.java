a603016103eaa9fe316a656681a7bc1b7aee4cb4
Hossein <www.jokerhpp@gmail.com>
2020-12-30 23:30:20 +0330

    first commit

---
+++ src/A.java
@@ -0,0 +1,9 @@
+
+public class A {
+	A(int a){
+		System.out.println(a);
+	}
+	void Show(){
+		System.out.println("20");
+	}
+}
---
+++ src/B.java
@@ -0,0 +1,15 @@
+
+public class B extends A{
+	
+	B(){
+		super(5);
+	}
+	
+	void run(){
+		super.Show();
+	}
+	
+	void Show(){
+		System.out.println("10");
+	}
+}
